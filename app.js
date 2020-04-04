cd00497cdd77a4173a735dfe8a47363f0fc48b7f
azlan <mhdazlanabaziz@gmai.com>
2020-04-04 13:50:11 +0800

    Third commit

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 let express = require('express');
 let app = express();
+let mongoose = require('mongoose')
+let Post = require('./models/posts').Post;
+
+mongoose.connect('mongodb://localhost/travels',{useNewUrlParser: true,useUnifiedTopology: true})
+let post1 = new Post({
+    id:5,
+    title: "Monggol Man",
+    date: new Date(),
+    description: "Heello Me",
+    text: "Hello World",
+    country: "Norway",
+    imageURL: "/images1.jpg"
+});
+
+post1.save();
 
 app.use(express.static('public'));
 
-app.listen(3000, ()=> console.log('System Server listening in 3000..'));
+app.listen(3000, () => console.log('System Server listening in 3000..'));
