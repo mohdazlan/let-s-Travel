bfeee4bd658b9aca19c6aaa896bf1c9d7aaa5a13
azlan <mhdazlanabaziz@gmai.com>
2020-04-05 15:44:48 +0800

    first insert data

--- app.js
+++ app.js
@@ -3,19 +3,44 @@ let app = express();
 let mongoose = require('mongoose')
 let Post = require('./models/posts').Post;
 
-mongoose.connect('mongodb://localhost/travels',{useNewUrlParser: true,useUnifiedTopology: true})
-let post1 = new Post({
-    id:5,
-    title: "Monggol Man",
-    date: new Date(),
-    description: "Heello Me",
-    text: "Hello World",
-    country: "Norway",
-    imageURL: "/images1.jpg"
-});
+mongoose.connect('mongodb://localhost/travels', { useNewUrlParser: true, useUnifiedTopology: true })
+app.use(express.json());
 
-post1.save();
+let id = 6;
+
+app.get("/posts", async (req, resp) => {
+    let posts = await Post.find();
+    resp.send(posts);
+})
+
+app.post("/posts", async (req, resp) => {
+    let reqBody = req.body;
+    let newPost = new Post({
+        id: id++,
+        title: reqBody.title,
+        date: new Date(),
+        description: reqBody.description,
+        text: reqBody.text,
+        country: reqBody.country,
+        imageURL: reqBody.imageUrl
+    })
+    await newPost.save();
+    resp.send('Created');
+})
+
 
 app.use(express.static('public'));
 
 app.listen(3000, () => console.log('System Server listening in 3000..'));
+
+
+// let post1 = new Post({
+//     id:5,
+//     title: "Monggol Man",
+//     date: new Date(),
+//     description: "Heello Me",
+//     text: "Hello World",
+//     country: "Norway",
+//     imageURL: "/images1.jpg"
+// });
+//post1.save();
---
+++ public/admin/js/create-post.js
@@ -0,0 +1,24 @@
+let createForm = document.querySelector('.create-post-form');
+let createTitle = document.querySelector('#create-title');
+let createCountry = document.querySelector('#create-country');
+let createImageUrl = document.querySelector('#create-image-url');
+let createText = document.querySelector('#create-text');
+
+createForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let text = createText.value;
+    fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: createTitle.value,
+            country: createCountry.value,
+            imageUrl: createImageUrl.value,
+            text: text,
+            description: text.substring(0, text.indexOf('.') + 1)
+        })
+    }).then((response) => response.text()).then((data) => console.log(data));
+
+})
